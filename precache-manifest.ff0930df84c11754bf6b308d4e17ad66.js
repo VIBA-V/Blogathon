cd0b4ed01f17cdcd770bec65e8ade28ab5088fd4
VIBA-V <avasyuthechnicals@gmail.com>
2020-05-26 20:47:47 +0530

    Updates

---
+++ precache-manifest.ff0930df84c11754bf6b308d4e17ad66.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ece68c7de641fe7cdf5fa779bde6b83",
+    "url": "/Blogathon/index.html"
+  },
+  {
+    "revision": "5e6295b95f1e1706733d",
+    "url": "/Blogathon/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "90eb20356abb9c92bb5c",
+    "url": "/Blogathon/static/css/main.1f493378.chunk.css"
+  },
+  {
+    "revision": "5e6295b95f1e1706733d",
+    "url": "/Blogathon/static/js/2.a7e1bd93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Blogathon/static/js/2.a7e1bd93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90eb20356abb9c92bb5c",
+    "url": "/Blogathon/static/js/main.934393ef.chunk.js"
+  },
+  {
+    "revision": "0420ade32fbd161ff188",
+    "url": "/Blogathon/static/js/runtime-main.56c546ea.js"
+  },
+  {
+    "revision": "df0b2ae65a471d9f7862fc8d4846b52e",
+    "url": "/Blogathon/static/media/1.df0b2ae6.png"
+  },
+  {
+    "revision": "2fe71c353a74e0e8ee85dc57d2a83af0",
+    "url": "/Blogathon/static/media/121.2fe71c35.png"
+  },
+  {
+    "revision": "ff7617db810d812203156270365e4454",
+    "url": "/Blogathon/static/media/2.ff7617db.png"
+  },
+  {
+    "revision": "4190ccdcb7f3e3eed3b3b46c31f75218",
+    "url": "/Blogathon/static/media/3.4190ccdc.png"
+  },
+  {
+    "revision": "0b68cf27e885270c07178aab6392279c",
+    "url": "/Blogathon/static/media/4.0b68cf27.png"
+  },
+  {
+    "revision": "064acde24eb530e300e3748ca9d3ed5f",
+    "url": "/Blogathon/static/media/47.064acde2.jpg"
+  },
+  {
+    "revision": "5eb2db29a1e5f7512b163a0699979fd4",
+    "url": "/Blogathon/static/media/5.5eb2db29.png"
+  },
+  {
+    "revision": "de0c9fbf3d2a3ab4893c1a18c8c6f5ec",
+    "url": "/Blogathon/static/media/blog.de0c9fbf.jpeg"
+  },
+  {
+    "revision": "a83da962cbcf368b799bf91ffcc24451",
+    "url": "/Blogathon/static/media/c.a83da962.png"
+  },
+  {
+    "revision": "eede3ce8c3bc28e3a8c798b7d7f7d442",
+    "url": "/Blogathon/static/media/cm.eede3ce8.jpg"
+  },
+  {
+    "revision": "5d9ab88a5361f8ca061ddfe9210fa627",
+    "url": "/Blogathon/static/media/dh.5d9ab88a.jpeg"
+  },
+  {
+    "revision": "ee1446351d567a9606d3c2d35f62bc52",
+    "url": "/Blogathon/static/media/f.ee144635.png"
+  },
+  {
+    "revision": "39d0a77da8148addd7186fb81938cc6c",
+    "url": "/Blogathon/static/media/g.39d0a77d.png"
+  },
+  {
+    "revision": "dd6bc4fac2bf81be1333757c2ef2d290",
+    "url": "/Blogathon/static/media/hs.dd6bc4fa.png"
+  },
+  {
+    "revision": "df76f9ffb661edf79b9ab2fc92d63994",
+    "url": "/Blogathon/static/media/hsh.df76f9ff.png"
+  },
+  {
+    "revision": "26c6dd6c7103366e2eae136985a3c40c",
+    "url": "/Blogathon/static/media/img.26c6dd6c.png"
+  },
+  {
+    "revision": "a2b4432956dad4e986fd0bdefbdf7b02",
+    "url": "/Blogathon/static/media/img2.a2b44329.png"
+  },
+  {
+    "revision": "028842b1875843a3892aec8d5b6478a4",
+    "url": "/Blogathon/static/media/m.028842b1.png"
+  },
+  {
+    "revision": "86084400c1fdbb0fefbc17de8dbbdd3e",
+    "url": "/Blogathon/static/media/sa.86084400.jfif"
+  },
+  {
+    "revision": "c0c1b8f351b9567ffdcc24867e574de8",
+    "url": "/Blogathon/static/media/v.c0c1b8f3.jpeg"
+  },
+  {
+    "revision": "619715560008465c9df2e28acdf864ad",
+    "url": "/Blogathon/static/media/vi.61971556.png"
+  }
+]);
